7912f14f0a11a1e17e733e41dffe47801c8bd7dc
Hacı Celal Aygar <celal.aygar@gmail.com>
2026-04-24 11:15:16 +0300

    Update image styling in project card component

--- components/cards/project-card.tsx
+++ components/cards/project-card.tsx
@@ -28,6 +28,7 @@ export function ProjectCard({ project }: ProjectCardProps) {
             <img
               src={project.image_url}
               alt={`${project.name} screenshot`}
+              style={{ filter: 'brightness(0.9)', mixBlendMode: 'multiply', height: '100%' }}
               className="w-full h-full object-cover object-center transition-transform duration-500 group-hover:scale-105"
             />
           ) : (
